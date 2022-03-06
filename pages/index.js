b10517491c413ea4313be62c10280c4b8f3ae2d5
PLag049 <100467974+PLag049@users.noreply.github.com>
2022-03-07 00:33:31 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,15 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width">
-  <link rel="stylesheet" href="style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<main>
-<h1 class="border">Абоба</h1>
-</mail>
-  )
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+   <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <body>
+    <h1 class="border">Абоба</h1>
+  </body>
+</html>
+
 }
